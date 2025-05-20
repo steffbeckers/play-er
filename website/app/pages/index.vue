b69dd97160c37c2d979283a9b5d0047efbeb6e97
Steff Beckers <steff@steffbeckers.com>
2025-05-20 20:57:54 +0200

    Logo Play-ER op website

--- website/app/pages/index.vue
+++ website/app/pages/index.vue
@@ -1,3 +1,30 @@
 <template>
-  <h1>Play-ER</h1>
+  <img src="/images/Play-ER.svg" alt="Play-ER logo" />
 </template>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  background-color: #fcfcfc;
+  display: flex;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#__nuxt {
+  align-items: center;
+  display: flex;
+  flex: 1 1 auto;
+  justify-content: center;
+}
+
+img {
+  flex: 0 0 auto;
+  margin: 1rem;
+  max-width: 600px;
+}
+</style>
