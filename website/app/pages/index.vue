3a6000516d4b5b41cd798245b278380f5ee386be
Steff Beckers <steff@steffbeckers.com>
2025-08-03 11:28:13 +0200

    website on main header

--- website/app/pages/index.vue
+++ website/app/pages/index.vue
@@ -1,30 +1,29 @@
 <template>
-  <img src="/images/Play-ER.svg" alt="Play-ER logo" />
+  <header class="main w-full">
+    <div class="mx-auto max-w-6xl">
+      <header class="flex justify-between h-20">
+        <img src="~/assets/images/P_Dark.svg" alt="Play-ER Logo" width="56" />
+        <nav class="flex text-white items-center gap-6">
+          <div class="px-4">HOME</div>
+          <div class="px-4">RESERVEREN</div>
+          <div class="px-4">CONTACT</div>
+          <div class="px-4">GAMES</div>
+          <div class="px-4">CLUBS</div>
+        </nav>
+        <div class="social-media flex gap-6">
+          <img src="~/assets/images/facebook.svg" alt="Play-ER Facebook" width="32" />
+          <img src="~/assets/images/instagram.svg" alt="Play-ER Instagram" width="32" />
+        </div>
+      </header>
+      <img src="~/assets/images/Play-ER_Dark.svg" alt="Play-ER Logo" width="660" />
+    </div>
+  </header>
+  <section class="w-full"></section>
+  <footer class="main w-full"></footer>
 </template>
 
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  background-color: #fcfcfc;
-  display: flex;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-
-#__nuxt {
-  align-items: center;
-  display: flex;
-  flex: 1 1 auto;
-  justify-content: center;
-}
-
-img {
-  flex: 0 0 auto;
-  margin: 1rem;
-  max-width: 600px;
+<style lang="css">
+header.main {
+  background-color: #323031;
 }
 </style>
